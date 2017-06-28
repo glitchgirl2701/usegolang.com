e36936f4edcbd79714b7714f339300b1a4481c29
Kayla Thomsen <kaylathomsen@Kaylas-MacBook-Pro.local>
2017-06-28 11:27:30 -0400

    chapter 10

--- controllers/users.go
+++ controllers/users.go
@@ -4,41 +4,71 @@ import (
   "usegolang.com/views"
   "fmt"
   "net/http"
+  "usegolang.com/models"
 )
 
-func NewUsers() *Users {
+func NewUsers(us models.UserService) *Users {
   return &Users {
-    NewView: views.NewView(
-      "bootstrap",
-      "views/users/new.gohtml")}
+    NewView:      views.NewView("bootstrap", "views/users/new.gohtml"),
+    LoginView:    views.NewView("bootstrap", "views/users/login.gohtml"),
+    UserService:  us,
+  }
 }
 
 type Users struct {
-  NewView *views.View
+  NewView     *views.View
+  LoginView   *views.View
+  models.UserService
 }
 
 type SignupForm struct {
+  Name      string  `schema:"name"`
   Email     string `schema:"email"`
   Password  string `schema:"password"`
 }
 
-//New is used to render the form where a user can
-//create a new user account.
-//
-// GET /signup
+type LoginForm struct {
+  Email    string `schema:"email"`
+  Password string `schema:"password"`
+}
+
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+  form := LoginForm{}
+  if err := parseForm(r, &form); err != nil {
+    panic(err)
+  }
+
+  user, err := u.UserService.Authenticate(form.Email, form.Password)
+  switch err {
+  case models.ErrNotFound:
+    fmt.Fprintln(w,  "Invalid email address.")
+  case models.ErrInvalidPassword
+    fmt.Fprintln(w, "Invalid password provided.")
+  case nil:
+    fmt.Fprintln(w, user)
+  default:
+    http.Error(w, err.Error(), http.StatusInterna;StatusInternalServerError)
+  }
+}
+
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
   if err := u.NewView.Render(w, nil); err != nil {
     panic(err)
   }
 }
 
-//Create is used to process the signup form when a users
-//submits it. THis is used to create a new user account.
-//
-//POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-  var form SignupForm
+  form := SignupForm{}
   if err := parseForm(r, &form); err != nil {
-    panic(err)}
-  fmt.Fprintln(w, form)
+    panic(err)
+  }
+  user := models.User{
+    Name:       form.Name,
+    Email:      form.Email,
+    Password:   form.Password,
+  }
+  if err := u.UserService.Create(user); err != nil {
+    panic(err)
+  }
+  fmt.Fprintln(w, user)
 }
